3bb60e8e8a386c65b0f2c69d6bd0e2e2d614f67e
Robin Edbom <robin.edbom@gmail.com>
2017-06-30 23:15:39 +0200

    Initial commit

---
+++ DrinkingGame/MainActivity.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Widget;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Com.Lilarcor.Cheeseknife;
+
+namespace DrinkingGame
+{
+    [Activity(Label = "DrinkingGame", MainLauncher = true, Icon = "@drawable/icon",Theme = "@style/Theme.AppCompat")]
+    public class MainActivity : Activity
+    {
+        [InjectView(Resource.Id.i_have_never)] private Switch _iHaveNeverSwitch;
+        [InjectView(Resource.Id.all_that_have)] private Switch _allThatHaveSwitch;
+        [InjectView(Resource.Id.who)] private Switch _whoSwitch;
+        [InjectView(Resource.Id.start_game)] private Button _startGameButton;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            Cheeseknife.Inject(this);
+        }
+
+        [InjectOnClick(Resource.Id.start_game)]
+        private void StartGameButtonOnClick(object sender, EventArgs e)
+        {
+            var questions = new List<string>();
+
+            if (_iHaveNeverSwitch.Checked)
+                questions.AddRange(Resources.GetStringArray(Resource.Array.i_have_never));
+
+            if (_whoSwitch.Checked)
+                questions.AddRange(Resources.GetStringArray(Resource.Array.who));
+
+            if (_allThatHaveSwitch.Checked)
+                questions.AddRange(Resources.GetStringArray(Resource.Array.all_that_have));
+
+            if (questions.Count == 0)
+            {
+                Snackbar.Make(_startGameButton, "No questions selected", Snackbar.LengthLong).Show();
+                return;
+            }
+            
+            var activity = new Intent (this, typeof(QuestionActivity));
+            activity.PutExtra("questions",questions.ToArray());
+            StartActivity (activity);
+        }
+    }
+}
---
+++ DrinkingGame/QuestionActivity.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Android.App;
+using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Widget;
+using Com.Lilarcor.Cheeseknife;
+using DrinkingGame.Extensions;
+
+namespace DrinkingGame
+{
+    [Activity(Label = "DrinkingGame", Icon = "@drawable/icon",Theme = "@style/Theme.AppCompat")]
+    public class QuestionActivity : Activity
+    {
+        
+        [InjectView(Resource.Id.forwardBtn)] private Button _forward;
+        [InjectView(Resource.Id.backBtn)] private Button _back;
+        [InjectView(Resource.Id.questionText)] private TextView _questionText;
+
+        private string[] _questions;
+        private int _index;
+        
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Question);
+            Cheeseknife.Inject(this);
+
+            _questions = Intent.Extras.GetStringArray("questions").ToArray();
+
+            //scramble the questions array
+            new Random().Shuffle(_questions);
+            
+            _questionText.Text = _questions[_index++];
+        }
+        
+        [InjectOnClick(Resource.Id.backBtn)]
+        private void BackButtonOnClick(object sender, EventArgs e)
+        {
+            if (_index == 0)
+            {
+                Snackbar.Make(_back, "No more questions",Snackbar.LengthLong).Show();
+                return;
+            }
+            _questionText.Text = _questions[_index--];
+        }
+        
+        [InjectOnClick(Resource.Id.forwardBtn)]
+        private void ForwardButtonOnClick(object sender, EventArgs e)
+        {
+            if (_index == _questions.Length -1)
+            {
+                _questionText.Text = "HOPE YOU ARE FUCKING DRUNK NOW BBITCH!";
+                Snackbar.Make(_forward, "No more questions",Snackbar.LengthLong).Show();
+                return;
+            }
+            _questionText.Text = _questions[_index++];
+        }
+    }
+}
